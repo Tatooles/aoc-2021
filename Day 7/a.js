e0081cb52434d900802e2d48656b62ac7088e6ce
Kevin Tatooles <tatooles@wisc.edu>
2022-12-07 20:05:04 -0600

    Finish a

--- Day 7/a.js
+++ Day 7/a.js
@@ -5,16 +5,16 @@ const filename = 'input.txt';
 const data = await fsPromises.readFile(filename, 'utf-8');
 const arr = data.split(/\n/);
 
-console.log(arr);
+// console.log(arr);
 
-const executeCommand = (line) => {
+const executeCommand = (tokens) => {
   // Execute command
-  if (line.charAt(2) === 'c') {
+  if (tokens[1] === 'cd') {
     // Change directory (move curr)
-    if (line.charAt(5) === '/') {
+    if (tokens[2] === '/') {
       curr = root;
     }
-    if (line.charAt(5) === '.') {
+    if (tokens[2] === '..') {
       if (curr.parent === null) {
         console.log('ERROR: null parent');
       } else {
@@ -23,9 +23,15 @@ const executeCommand = (line) => {
       }
     } else {
       // Move into directory, find which child to switch into
+      for (const child of curr.children) {
+        if (child.name === tokens[2]) {
+          curr = child;
+          return;
+        }
+      }
     }
   }
-  else if (line.charAt(2) === 'l') {
+  else if (tokens[1] === 'ls') {
     // I think we can just ignore ls, could maybe set a flag for read mode but that can be implied
   }
   else {
@@ -34,19 +40,51 @@ const executeCommand = (line) => {
 }
 
 // root directory aka head of the tree
-const root = { name: null, parent: null, children: [], size: -1 };
+const root = { name: null, parent: null, children: [], size: 0 };
 
 let curr = root; // Our current spot in the directory
 
 for (const line of arr) {
-  if (line.charAt(0) === '$') {
-    executeCommand(line);
+  const tokens = line.split(' ');
+  if (tokens[0] === '$') {
+    executeCommand(tokens);
   }
-  else if (line.charAt(0) === 'd') { // We have a directory
+  else if (tokens[0] === 'dir') { // We have a directory
     // Add directory to it's parent
-    // curr.children.push({ name: 'name', })
+    curr.children.push({ name: tokens[1], parent: curr, children: [], size: 0 });
   }
   else { // We have a file
+    let size = parseInt(tokens[0])
     // Add file to it's directory
+    curr.size += size;
+
+    // Gotta also add to all parent directories
+    let i = curr;
+    while (i.parent != null) {
+      i = i.parent;
+      i.size += size;
+    }
   }
 }
+
+let answer = 0;
+
+// Do a full tree traversal to figure out this answer
+const traverseTree = (root) => {
+  if (root.size <= 100000) {
+    answer += root.size;
+  }
+  // Base case
+  // no children
+  if (root.children.length === 0) {
+    return;
+  } else {
+    for (const child of root.children) {
+      traverseTree(child);
+    }
+  }
+}
+
+traverseTree(root);
+
+console.log('answer', answer);
