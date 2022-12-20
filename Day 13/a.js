baecef12a78fae08f50a878f51643370caaca3aa
Kevin Tatooles <tatooles@wisc.edu>
2022-12-19 23:03:11 -0600

    Working on comparison function

--- Day 13/a.js
+++ Day 13/a.js
@@ -32,23 +32,51 @@ const parsePacket = (inputString) => {
       currentArray = parent;
     } else {
       // Add to array
-      currentArray.push(c);
+      currentArray.push(parseInt(c));
     }
   }
   return packet;
 }
 
+const checkOrder = (left, right) => {
+  while (left.length != 0 && right.length != 0) { // TODO: Need a different length check
+    let leftVal = left[0];
+    let rightVal = right[0];
+    if (!isNaN(leftVal) && !isNaN(rightVal)) {
+      if (leftVal < rightVal) {
+        // Correct order
+        return true;
+      } else if (leftVal == rightVal) {
+        left.shift();
+        right.shift();
+      } else {
+        return false;
+      }
+    }
+    else if (isNaN(leftVal) && isNaN(rightVal)) {
+      // Both arrays, iterate through the arrays
+      // Try recursive lol
+      return checkOrder(leftVal, rightVal);
+    }
+    else {
+      // One is an array
+    }
+  }
+  return false;
+}
+
+let sum = 0;
+
 // Want to read pairs of lines together
 for (let i = 0; i < arr.length; i += 3) {
-  const firstPacket = arr[i];
-  const secondPacket = arr[i + 1];
-
-  const second = [];
-
   // First convert the input string into a nested array
-  const first = parsePacket(firstPacket.slice(1, -1));
-  console.log(first);
+  const left = parsePacket(arr[i].slice(1, -1));
+  const right = parsePacket(arr[i + 1].slice(1, -1));
 
+  // console.log(left);
   // TODO: Determine if they are in the right order
   // Prob want another function
+  const value = checkOrder(left, right);
+  if (value) sum += i; // This is wrong
+  console.log(i, value);
 }
