26d586afcbec39d276c84abb4871d6b1616d13ab
Kevin Tatooles <tatooles@wisc.edu>
2022-12-22 20:14:14 -0600

    Final 13 part 2 solution

--- Day 13/b.js
+++ Day 13/b.js
@@ -1,45 +1,37 @@
 import { promises as fsPromises } from 'fs';
 
 // Read text file, separating by line
-const filename = 'test.txt';
+const filename = 'input.txt';
 const data = await fsPromises.readFile(filename, 'utf-8');
 const arr = data.split(/\n/);
 
 const checkOrder = (left, right) => {
-  while (left.length != 0) {
-    if (right.length === 0) return false;
-    let leftVal = left[0];
-    let rightVal = right[0];
-    if (typeof leftVal === 'number' && typeof rightVal === 'number') {
-      if (leftVal < rightVal) return true; // Correct order
-      else if (leftVal === rightVal) {
-        left.shift();
-        right.shift();
-      }
-      else return false; // Wrong order
+  if (typeof left === 'number' && typeof right === 'number') {
+    if (left < right) return true; // Correct order
+    else if (left > right) return false; // Wrong order
+    else return 0;
+  }
+  else if (typeof left !== 'number' && typeof right !== 'number') {
+    // Both arrays, iterate through the arrays
+    for (let i = 0; i < left.length && i < right.length; i++) {
+      const result = checkOrder(left[i], right[i]);
+      if (result !== 0) return result;
     }
-    else if (typeof leftVal !== 'number' && typeof rightVal !== 'number') {
-      // Both arrays, iterate through the arrays
-      let result = checkOrder(leftVal, rightVal);
-      if (result !== 0) return result; // Otherwise continue
-      left.shift();
-      right.shift();
+
+    if (left.length < right.length) return true;
+    else if (left.length > right.length) return false;
+    return 0;
+  }
+  else {
+    // One is an array
+    if (typeof left === 'object') {
+      // Left is an array
+      return checkOrder(left, [right]);
     }
     else {
-      // One is an array
-      if (typeof leftVal === 'object') {
-        // Left is an array
-        let result = checkOrder(leftVal, [rightVal]);
-        if (result !== 0) return result; // Otherwise continue
-      }
-      else {
-        let result = checkOrder([leftVal], rightVal);
-        if (result !== 0) return result; // Otherwise continue
-      }
+      return checkOrder([left], right);
     }
   }
-  if (right.length > 0) return true;
-  return 0; // Can't say
 }
 
 let all = [];
@@ -54,28 +46,17 @@ for (let i = 0; i < arr.length / 3; i++) {
 all.push([[2]]);
 all.push([[6]]);
 
-console.log(all.length);
-
 all.sort((a, b) => {
-  const inorder = checkOrder(structuredClone(a), structuredClone(b));
-  if (!inorder) {
-    return 1;
-  } else {
-    return -1;
-  }
+  if (!checkOrder(a, b)) return 1;
+  return -1;
 });
 
 let twoIndex = 0;
 let sixIndex = 0;
 
 all.forEach((element, i) => {
-  console.log(element);
-  if (JSON.stringify(element) === '[[2]]') {
-    twoIndex = i;
-  }
-  if (JSON.stringify(element) === '[[6]]') {
-    sixIndex = i;
-  }
-})
+  if (JSON.stringify(element) === '[[2]]') twoIndex = i + 1;
+  if (JSON.stringify(element) === '[[6]]') sixIndex = i + 1;
+});
 
 console.log('anwer', twoIndex * sixIndex);
